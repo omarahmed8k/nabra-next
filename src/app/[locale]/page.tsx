6e4b46feb1378cf51f58b109d23930e41c9c776d
Omar Abdelrahman <omar@rwaj.co>
2025-11-15 17:54:35 +0200

    refactor: remove About, FAQ, and CTASection components from Home page

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -2,10 +2,7 @@ import Hero from "@/components/home/hero";
 import Video from "@/components/home/video";
 import Partners from "@/components/home/partners";
 import Services from "@/components/home/services";
-import About from "@/components/home/about";
 import Testimonials from "@/components/home/testimonials";
-import FAQ from "@/components/home/faq";
-import CTASection from "@/components/home/cta-section";
 import Projects from "@/components/home/projects";
 
 export default function Home() {
@@ -16,10 +13,7 @@ export default function Home() {
       <Partners />
       <Services />
       <Projects />
-      <About />
       <Testimonials />
-      <FAQ />
-      <CTASection />
     </>
   );
 }
